76f6d72a4fb8a9c6d71e34a374e6d6d001c26848
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-12-26 21:53:48 +0200

    add todos

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const app = express();
-const port = 3000;
+//const express = require('express');
+//const bodyParser = require('body-parser');
+//const cors = require('cors');
+//const app = express();
+//const port = 3000;
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -11,15 +11,15 @@ app.use(bodyParser.json());
 app.use(cors());
 const users = ['Alex', 'Bobby'];
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 app.get('/users', (req, res) => {
-  res.json(users);
+    res.json(users);
 });
 app.post('/users', (req, res) => {
-  users.push(req.body.name);
-  res.json(users);
+    users.push(req.body.name);
+    res.json(users);
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`);
+}); //
--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
-//import itemsTemplate from './templates/menu.hbs';
-//import './mymain.js';
-//import './styles.css';
-fetch('http://localhost:3000/users')
-  .then(data => data.json())
-  .then(console.log())
-  .catch(error => console.log('failed to fetch'));
+import toastr from 'toastr';
+import todoItemTemplate from './templates/todoitems.hbs';
+import options from './toastr.options';
+import 'toastr/build/toastr.min.css';
+import './styles.css';
+toastr.options = options;
+import savedTodos from './todos.json';
+let todos = [];
+
+const refs = {
+    inputForm: document.querySelector('.input-form'),
+    todoList: document.querySelector('.todo-list'),
+    loader: document.querySelector('.loader'),
+};
+const renderList = () => {
+    refs.todoList.innerHTML = '';
+    refs.todoList.insertAdjacentHTML(
+        'beforeend',
+        todos.map(todoItemTemplate).join(''),
+    );
+};
+const showLoader = () => {
+    refs.loader.classList.add('show');
+};
+const hideLoader = () => {
+    refs.loader.classList.remove('show');
+};
+const showList = () => {
+    refs.todoList.classList.add('show');
+};
+const hideList = () => {
+    refs.todoList.classList.remove('show');
+};
+const disableForm = () => {
+    refs.inputForm.querySelector('.input').setAttribute('disabled', true);
+    refs.inputForm.querySelector('.button').setAttribute('disabled', true);
+};
+const anableForm = () => {
+    refs.inputForm.querySelector('.input').removeAttribute('disabled');
+    refs.inputForm.querySelector('.button').removeAttribute('disabled');
+};
+const handleSubmit = e => {
+    console.log(e);
+    const target = e.target.elements.text;
+    const text = target.value;
+    e.preventdefault();
+    if (!text) {
+        return;
+    }
+    disableForm();
+    todos.push({
+        text: text,
+        idDone: false,
+    });
+    renderList();
+    anableForm();
+};
+const loadTodos = () => {
+    // disableForm();
+    // hideList();
+    // showLoader();
+    renderList();
+    //   hideLoader();
+    //   showList();
+    //   anableForm();
+};
+refs.inputForm.addEventListener('submit', handleSubmit);
+//refs.todoList.addEventListener('click', handleTodosClick);
+loadTodos();
