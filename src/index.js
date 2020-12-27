087076668c88687466034332907c712d5db31479
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2020-12-28 00:48:32 +0200

    add shablon

--- src/index.js
+++ src/index.js
@@ -1,69 +1,20 @@
 import toastr from 'toastr';
-import todoItemTemplate from './templates/todoitems.hbs';
+import countriesItemTemplate from './templates/countries.hbs';
 import options from './toastr.options';
 import 'toastr/build/toastr.min.css';
 import './styles.css';
 toastr.options = options;
-import savedTodos from './todos.json';
-let todos = [];
-
+const url = 'https://restcountries.eu/rest/v2/name/united';
+fetch(url)
+    .then(res => {
+        if (res.ok) return res.json();
+        throw new Error('Error fetching data');
+    })
+    .then(data => console.log(data))
+    .catch(err => {
+        console.error('Error:  ', err);
+    });
 const refs = {
     inputForm: document.querySelector('.input-form'),
-    todoList: document.querySelector('.todo-list'),
-    loader: document.querySelector('.loader'),
+    countriesList: document.querySelector('.text-countries'),
 };
-const renderList = () => {
-    refs.todoList.innerHTML = '';
-    refs.todoList.insertAdjacentHTML(
-        'beforeend',
-        todos.map(todoItemTemplate).join(''),
-    );
-};
-const showLoader = () => {
-    refs.loader.classList.add('show');
-};
-const hideLoader = () => {
-    refs.loader.classList.remove('show');
-};
-const showList = () => {
-    refs.todoList.classList.add('show');
-};
-const hideList = () => {
-    refs.todoList.classList.remove('show');
-};
-const disableForm = () => {
-    refs.inputForm.querySelector('.input').setAttribute('disabled', true);
-    refs.inputForm.querySelector('.button').setAttribute('disabled', true);
-};
-const anableForm = () => {
-    refs.inputForm.querySelector('.input').removeAttribute('disabled');
-    refs.inputForm.querySelector('.button').removeAttribute('disabled');
-};
-const handleSubmit = e => {
-    console.log(e);
-    const target = e.target.elements.text;
-    const text = target.value;
-    e.preventdefault();
-    if (!text) {
-        return;
-    }
-    disableForm();
-    todos.push({
-        text: text,
-        idDone: false,
-    });
-    renderList();
-    anableForm();
-};
-const loadTodos = () => {
-    // disableForm();
-    // hideList();
-    // showLoader();
-    renderList();
-    //   hideLoader();
-    //   showList();
-    //   anableForm();
-};
-refs.inputForm.addEventListener('submit', handleSubmit);
-//refs.todoList.addEventListener('click', handleTodosClick);
-loadTodos();
