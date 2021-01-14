1c54c62766b0197a559cd3d6c83d2670c4f1eecd
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-14 23:43:36 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import itemsElement from './menu.json';
-import itemsTemplate from './templates/menu.hbs';
-import './mymain.js';
-import './styles.css';
-const menu = document.querySelector('.js-menu');
-const makup = itemsTemplate(itemsElement);
-menu.insertAdjacentHTML('beforeend', makup);
