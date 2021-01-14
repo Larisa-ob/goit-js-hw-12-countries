4dd5fdd568e7a4c8b9cbda9d953892d239bdfb52
Larisa-ob <66275364+Larisa-ob@users.noreply.github.com>
2021-01-14 23:40:06 +0200

    not error

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import toastr from 'toastr';
-import countriesItemTemplate from './templates/countries.hbs';
-import options from './toastr.options';
-import 'toastr/build/toastr.min.css';
 import './styles.css';
-toastr.options = options;
-const url = 'https://restcountries.eu/rest/v2/name/united';
-fetch(url)
-    .then(res => {
-        if (res.ok) return res.json();
-        throw new Error('Error fetching data');
-    })
-    .then(data => console.log(data))
-    .catch(err => {
-        console.error('Error:  ', err);
-    });
-const refs = {
-    inputForm: document.querySelector('.input-form'),
-    countriesList: document.querySelector('.text-countries'),
-};
+import './js/fetchCountries';
+import './js/country_up';
+import './js/pnotify';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
---
+++ src/js/country_up.js
@@ -0,0 +1,45 @@
+import fetchCountries from './fetchCountries';
+import { success, info, error } from './pnotify.js';
+import oneCountryMarkup from '../templates/countries.hbs';
+import upToTenCountriesMarkup from '../templates/ten_countries.hbs';
+
+const refs = {
+    input: document.querySelector('#js-input'),
+    output: document.querySelector('#js-output'),
+};
+const _ = require('lodash');
+const url = 'https://restcountries.eu/rest/v2/name/';
+
+const debouncedSearch = _.debounce(() => {
+    if (refs.input.value !== '') {
+        fetchCountries(url + refs.input.value.toLowerCase())
+            .then(data => {
+                searchMarkup(data);
+            })
+            .catch(e => {
+                error({
+                    title: 'Ooops! Something went wrong 🤷',
+                    text: e.message,
+                    type: 'error',
+                });
+                refs.output.innerHTML = '';
+            });
+    }
+}, 500);
+
+const searchMarkup = function (data) {
+    if (data.length === 1) {
+        refs.output.innerHTML = oneCountryMarkup(data);
+    } else if ((data.length > 1) & (data.length <= 10)) {
+        refs.output.innerHTML = upToTenCountriesMarkup(data);
+    } else if (data.length > 10) {
+        error({
+            title: 'Too many matches found!',
+            text: 'Please enter a more specific query.',
+            type: 'error',
+        });
+        refs.output.innerHTML = '';
+    }
+};
+refs.input.addEventListener('input', debouncedSearch);
+refs.input.addEventListener('submit', event => event.preventDefault());
